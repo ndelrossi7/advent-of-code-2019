8bf14c4dfdd4444e73c4c8c70ef72259f8a4409d
Natalie Del Rossi <natalie.delrossi@flatironschool.com>
2019-12-03 15:44:38 -0500

    day1

---
+++ dec_1/dec1_solution.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+masses = np.loadtxt('dec_1_input.txt', delimiter = ',', unpack = False)
+
+def fuel_counter_upper(masses):
+    fuel = 0
+    for mass in masses:
+        fuel += (mass//3) - 2
+    return int(fuel)
